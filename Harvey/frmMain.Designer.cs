03f7ccdc6fd7dee1faa2a960d3cb42ae13b0650f
secretgeek <secretgeek@localhost>
2012-11-17 23:10:45 +1000

    first commit

---
+++ Harvey/frmMain.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Harvey
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtConversation = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMessage.Location = new System.Drawing.Point(12, 6);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(183, 40);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(264, 43);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(20, 25);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "x";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // txtConversation
+            // 
+            this.txtConversation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConversation.Location = new System.Drawing.Point(0, 43);
+            this.txtConversation.Multiline = true;
+            this.txtConversation.Name = "txtConversation";
+            this.txtConversation.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtConversation.Size = new System.Drawing.Size(284, 155);
+            this.txtConversation.TabIndex = 6;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(209, 6);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(63, 40);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "user";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnSend);
+            this.panel2.Controls.Add(this.txtMessage);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 198);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(284, 64);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtUserName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 43);
+            this.panel1.TabIndex = 4;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUserName.Location = new System.Drawing.Point(144, 12);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(128, 20);
+            this.txtUserName.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtConversation);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmMain";
+            this.Text = "Harvey";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtConversation;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtUserName;
+    }
+}
+
---
+++ Harvey/frmMain.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Harvey
+{
+    //Major hat-tip to Derek Greer's 'RabbitMQ for Windows' articles http://lostechies.com/derekgreer/tag/rabbitmq/
+    //To setup a host server for your chatting...
+    //1. install erlang: http://www.erlang.org/download.html
+    //2. set the ERLANG_HOME environment variable to point to the erlang folder under program files. e.g. C:\Program Files\erl5.9.2
+    //3. Install rabbitMQ: http://www.rabbitmq.com/download.html
+    //4. enable the rabbitmq management plugin. from an elevated cmd prompt, go to rabbit's sbin folder (e.g. %programfiles%\RabbitMQ Server\rabbitmq_server-2.8.7\sbin") and run: "rabbitmq-plugins.bat enable rabbitmq_management"
+    //5. To activate the management plugin, stop, install and start the rabbitmq service:
+    //           rabbitmq-service.bat stop 
+    //           rabbitmq-service.bat install
+    //           rabbitmq-service.bat start 
+    //6. Visit http://localhost:55672/mgmt/ and see that your rabbitMQ instance is alive
+
+    public partial class frmMain : Form
+    {
+        string exchangeName = "chatter";
+        string clientId = Guid.NewGuid().ToString();
+        IConnection connection = null;
+        IModel channelSend = null;
+        IModel channelReceive = null;
+        Thread receivingThread = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            txtUserName.Text = clientId.Substring(0, 13); //to truncate a guid is considered quite a funny joke where i come from.
+            txtConversation.Text = "Do not share credit card numbers unless the person is quite convincing or insistent.\r\n";
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/"
+            };
+
+            connection = connectionFactory.CreateConnection();
+            channelSend = connection.CreateModel();
+            channelSend.ExchangeDeclare(exchangeName, ExchangeType.Fanout, false, true, null);
+            channelReceive = connection.CreateModel();
+            channelReceive.QueueDeclare(clientId, false, false, true, null);
+            channelReceive.QueueBind(clientId, exchangeName, "");
+            receivingThread = new Thread(() => channelReceive.StartConsume(clientId, MessageHandler));
+            receivingThread.Name = "ReceivingThread"; //name the thread so that when it goes insane you will be able to apportion blame.
+            receivingThread.Start();
+            txtMessage.Focus();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string input = txtUserName.Text + " > " + txtMessage.Text;
+            byte[] message = Encoding.UTF8.GetBytes(input);
+            channelSend.BasicPublish(exchangeName, "", null, message);
+            txtMessage.Text = string.Empty;
+            txtMessage.Focus();
+        }
+
+        public void MessageHandler(IModel channel, DefaultBasicConsumer consumer, BasicDeliverEventArgs eventArgs)
+        {
+            string message = Encoding.UTF8.GetString(eventArgs.Body) + "\r\n";
+
+            txtConversation.InvokeIfRequired(() =>
+            {
+                txtConversation.Text += message;
+                txtConversation.ScrollToEnd();
+            });
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtConversation.Text = "";
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (channelSend.IsOpen) channelSend.Close();
+            if (channelReceive.IsOpen) channelReceive.Close();
+            if (connection.IsOpen) connection.Close();
+        }
+
+        private void frmMain_ResizeEnd(object sender, EventArgs e)
+        {
+            txtConversation.ScrollToEnd();
+        }
+    }
+
+    public static class ChannelExtensions
+    {
+        public static void StartConsume(this IModel channel, string queueName, Action<IModel, DefaultBasicConsumer, BasicDeliverEventArgs> callback)
+        {
+            QueueingBasicConsumer consumer = new QueueingBasicConsumer(channel);
+            channel.BasicConsume(queueName, true, consumer);
+
+            while (true)
+            {
+                try
+                {
+                    var eventArgs = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                    new Thread(() => callback(channel, consumer, eventArgs)).Start();
+                }
+                catch (EndOfStreamException)
+                {
+                    // The consumer was cancelled, the model closed, or the connection went away.
+                    break;
+                }
+            }
+        }
+
+        //hat tip: http://stackoverflow.com/questions/2367718/c-automating-the-invokerequired-code-pattern
+        public static void InvokeIfRequired(this Control control, MethodInvoker action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+        public static void ScrollToEnd(this TextBox textbox)
+        {
+            textbox.Select(textbox.Text.Length - 1, 0);
+            textbox.ScrollToCaret();
+        }
+    }
+}
